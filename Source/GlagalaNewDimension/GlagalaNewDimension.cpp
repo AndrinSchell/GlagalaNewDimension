aac74d932a05076ecc423a89690449b429e74c8a
AndrinSchell <andrin.schellenberg@hotmail.com>
2024-11-10 15:54:21 +0100

    initial project commit
    This commit contains all unreal engine project files

---
+++ Source/GlagalaNewDimension/GlagalaNewDimension.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GlagalaNewDimension.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GlagalaNewDimension, "GlagalaNewDimension" );
