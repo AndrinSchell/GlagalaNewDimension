f90185565b473dfbe964f8612cd893ebd16d1682
sgaggleham <andrin.schellenberg@hotmail.com>
2024-11-26 12:40:16 +0100

    Updated Ingame menu system, added score

---
+++ Source/GlagalaNewDimension/System/GlagalaFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GlagalaFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class GLAGALANEWDIMENSION_API UGlagalaFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
